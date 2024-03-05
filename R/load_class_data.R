19e348c65aa25e1e6850a7f84990588ba13df13a
Daniboid <dlarran@purdue.edu>
2024-03-04 19:01:18 -0500

    srsly, wrk nau. k thnx

---
+++ R/load_class_data.R
@@ -0,0 +1,13 @@
+#'
+#' Load the Class Data for Examples
+#'
+#'
+#' @description
+#' Load the class data to the environment for use.
+#'
+#'
+#' @export load_class_data
+
+load_class_data = function(){
+  load("./R/ClassData.RData")
+}
