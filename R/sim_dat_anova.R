42ad69d49b1d25e53f4e971e33aa36ab21128c63
Daniboid <dlarran@purdue.edu>
2024-10-24 18:31:10 -0400

    Update sim_dat_anova.R

--- R/sim_dat_anova.R
+++ R/sim_dat_anova.R
@@ -33,13 +33,15 @@ sim_dat_anova = function(aov_in,
       tmp_sd = stats::sd(aov_in$model[,iv])   # and standard deviation...
       df_sim[,iv] = stats::rnorm(n_forSim, tmp_mu, tmp_sd) # use those to generate a simulated sample...
     } else {
-      tmp_ratio = table(aov_in$model[,iv])/sum(table(aov_in$model[,iv]))
-      tmp_vect  = rep(names(tmp_ratio), round(tmp_ratio*n_forSim))
-      if (length(tmp_vect) < nrow(df_sim)) tmp_vect = c(tmp_vect, rep(names(tmp_ratio[tmp_ratio == max(tmp_ratio)]),
-                                                                      nrow(df_sim) - length(tmp_vect)))
-      if(length(tmp_vect) > nrow(df_sim)) tmp_vect = tmp_vect[-length(tmp_vect)]
-      df_sim[,iv] = sample(factor(tmp_vect))
-    } # otherwise assume the same number of nominal and ordinal observations in the simulated sample.
+      if (n_forSim == nrow(aov_in$model)) df_sim[,iv] = aov_in[,iv] else {
+        tmp_ratio = table(aov_in$model[,iv])/sum(table(aov_in$model[,iv]))
+        tmp_vect  = rep(names(tmp_ratio), round(tmp_ratio*n_forSim))
+        if (length(tmp_vect) < nrow(df_sim)) tmp_vect = c(tmp_vect, rep(names(tmp_ratio[tmp_ratio == max(tmp_ratio)]),
+                                                                        nrow(df_sim) - length(tmp_vect)))
+        if(length(tmp_vect) > nrow(df_sim)) tmp_vect = tmp_vect[-length(tmp_vect)]
+        df_sim[,iv] = sample(factor(tmp_vect))
+      }
+    } # otherwise assume the same ratio of nominal and ordinal observations in the simulated sample, but randomize them.
   }
 
   df_sim$perf_pred_dv = stats::predict.lm(aov_in, newdata=df_sim)
