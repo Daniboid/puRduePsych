6ed1f5133724d29a1f11fbf03fb216c8ddb82410
Daniboid <daniel.larranaga.87@my.csun.edu>
2025-02-28 12:55:39 -0500

    Create pairwise_post_hocs_anova.R

---
+++ R/pairwise_post_hocs_anova.R
@@ -0,0 +1,17 @@
+#' Pairwise Comparisons for ANOVA
+#'
+#' @description
+#' Not functional yet. But hopefully it soon will be...
+#'
+#'
+#'
+#'
+#'
+
+
+
+pairwise_post_hocs_anova = function(aov_in,
+                                    ss_type = "1",
+                                    comp_type = c("Tukey", "LSD", "Bonferroni")) {
+
+}
